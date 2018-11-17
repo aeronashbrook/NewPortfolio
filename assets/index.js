f3feabf6e3c6833ca56904eac49dc4d3634ad65b
Aeron Ashbrook <aeronashbrook@stp-er-10-129-14-173.wireless.umn.edu>
2018-11-17 11:03:08 -0600

    index responsive update

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $("#aboutborder").hide();
     $("#contactborder").hide();
+    $("#container").hide();
+
+    $("#container").fadeIn(2000).show();
 
     $("#about").hover(function(){
         $("#aboutborder").show();
